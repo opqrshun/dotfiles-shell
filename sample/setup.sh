d8b1fafa044ca1266a5bbd443f90fa7cdbf7047d
ttaki <opqrshun@gmail.com>
2020-05-18 10:23:28 +0900

    Update setup.sh

--- sample/setup.sh
+++ sample/setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-git clone https://github.com/ttaki/dotfiles-shell .dotfiles-shell
-bash .dotfiles-shell/install.sh
+git clone https://github.com/ttaki/dotfiles-shell $HOME/.dotfiles-shell
+bash $HOME/.dotfiles-shell/install.sh
